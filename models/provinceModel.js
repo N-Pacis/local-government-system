91ed874e1cf499cfd78680c236332c6eeedd954e
pacis <pacis@gmail.com>
2021-02-11 08:56:41 +0200

    local government system

--- models/provinceModel.js
+++ models/provinceModel.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 const Joi = require('joi');
-const {countrySchema} = require("../models/countryModel")
 
 function validateProvince(province){
     const schema = {
